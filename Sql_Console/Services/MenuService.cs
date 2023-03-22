4c4c8c74eb3d2d16b03a4ef167f55b01887c5d91
jocke94 <joakim94larsson@gmail.com>
2023-03-22 11:46:14 +0100

    Services branch

---
+++ Sql_Console/Services/MenuService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sql_Console.Services
+{
+    internal class Class1
+    {
+    }
+}
